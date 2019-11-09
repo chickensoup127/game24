01c4f37d9675b0d00e18b0688f64a68a448e6659
kizhod <54351020+kizhod@users.noreply.github.com>
2019-11-09 11:40:28 +0900

    spawner 구현

---
+++ 졸업런/Assets/Script/Entity/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : Entity
+{
+    float dt = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //GenMonster();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+           
+    }
+
+    void GenMonster()
+    {
+        GameObject prefab = Resources.Load("Enemy_Boss") as GameObject;
+        GameObject Enemy = Instantiate(prefab);
+        Enemy.transform.position = transform.position;
+    }
+}
