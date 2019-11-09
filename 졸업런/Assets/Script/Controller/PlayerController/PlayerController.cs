0f5c6e19b57034d8bff0f3a2df37fad9e04e8307
홍태하 <xogkdmltaha1@naver.com>
2019-11-09 09:59:11 +0900

    기본 세팅

---
+++ 졸업런/Assets/Script/Controller/PlayerController/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : Controller
+{
+    public GameObject _player;
+    public float _speed=2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveV = Vector2.zero;
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            moveV.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            moveV.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            moveV.x += 1;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            moveV.y += 1;
+        }
+        moveV = moveV.normalized * _speed * Time.deltaTime;
+        _player.GetComponent<Entity>().Move(moveV);
+
+    }
+}
