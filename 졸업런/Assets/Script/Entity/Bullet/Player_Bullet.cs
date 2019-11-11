dd0e10b66ed4dba86539667ce4fe62eef1404c31
HongTaeha <xogkdmltaha1@gmail.com>
2019-11-11 14:12:09 +0900

    manager 추가

--- 졸업런/Assets/Script/Entity/Bullet/Player_Bullet.cs
+++ 졸업런/Assets/Script/Entity/Bullet/Player_Bullet.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : Entity
+public class Player_Bullet : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
--- 졸업런/Assets/Script/Manager/ObjectManager/ObjectManager.cs
+++ 졸업런/Assets/Script/Manager/ObjectManager/ObjectManager.cs
@@ -2,12 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : Entity
+public class ObjectManager : Manager
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
