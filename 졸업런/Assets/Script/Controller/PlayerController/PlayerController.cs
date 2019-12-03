3cbb9955d2b435722d5ebf86100857b09063adf2
홍태하 <xogkdmltaha1@naver.com>
2019-12-03 19:31:57 +0900

    애니메이션 작업중

--- 졸업런/Assets/Script/Controller/PlayerController/PlayerController.cs
+++ 졸업런/Assets/Script/Controller/PlayerController/PlayerController.cs
@@ -6,38 +6,66 @@ public class PlayerController : Controller
 {
     public GameObject _player;
     public float _speed=2.0f;
+    public GameObject UI_Pause;
+
     private Camera mainCamera; // 메인 카메라
     private bool check = true;
-    public GameObject UI_Pause;
-    Animator animator;
+    public Animator animator;
+    private Rigidbody2D rigid;
     // Start is called before the first frame update
     void Start()
     {
         mainCamera = GameObject.Find("Main Camera").GetComponent<Camera>();
+        rigid = _player.GetComponent<Rigidbody2D>();
+        
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector2 moveV = Vector2.zero;
+        
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        Vector3 moveV = Vector3.zero;
+        SpriteRenderer renderer = _player.GetComponentInChildren<SpriteRenderer>();
 
         if (Input.GetKey(KeyCode.S))
         {
+            //Vector3 temp= Vector3.down;
+            //moveV += temp;
+    
             moveV.y -= 1;
+            animator.SetInteger("Is_Move",3);
         }
         if (Input.GetKey(KeyCode.A))
         {
+            //Vector3 temp = Vector3.left;
+            //moveV += temp;
             moveV.x -= 1;
+            renderer.flipX = true;
+            animator.SetInteger("Is_Move", 1);
 
         }
         if (Input.GetKey(KeyCode.D))
         {
+            //Vector3 temp = Vector3.right;
+            //moveV += temp ;
             moveV.x += 1;
+
+            renderer.flipX = false;
+            animator.SetInteger("Is_Move", 1);
+
         }
         if (Input.GetKey(KeyCode.W))
         {
+            //Vector3 temp = Vector3.up;
+            //moveV += temp;
             moveV.y += 1;
+            animator.SetInteger("Is_Move", 2);
         }
+        
 
         if (Input.GetMouseButton(0)&&check&&Time.timeScale==1)
         {
@@ -47,6 +75,8 @@ public class PlayerController : Controller
             _player.GetComponent<Player>().ShootBullet(Mouseposition);
             StartCoroutine(WaitForIt());
         }
+        //rigid.AddForce(moveV, ForceMode2D.Impulse);
+        //transform.position += moveV*1.0f*Time.deltaTime;
         moveV = moveV.normalized * _speed * Time.deltaTime;
         _player.GetComponent<Entity>().Move(moveV);
 
