8b61ba533d612869405df50fbbc552acc4e60ef2
홍태하 <xogkdmltaha1@naver.com>
2019-12-08 21:32:25 +0900

    되돌리기

---
+++ 졸업런/Assets/Script/Entity/Entity.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+    public GameObject _visualObj;
+    private BattleValue _bv = null;
+    public BattleValue bv { get { return _bv; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void InitBattleValue(float hp, float atk)
+    {
+        _bv = new BattleValue(hp, atk);
+    }
+
+    public void Move(Vector2 moveVector)
+    {
+        transform.Translate(moveVector);
+        //transform.FindChild("body").gameObject.GetComponent<Rigidbody2D>().AddForce(moveVector);
+    }
+}
---
+++ 졸업런/Assets/Script/UI/UI_Playerstatus.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class UI_Playerstatus : MonoBehaviour
+{
+
+    public GameObject _player = null;
+    public Text _playerStatusText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (_player != null)
+        {
+            _playerStatusText.text = " X " + _player.GetComponent<Entity>().bv._hp;
+        }
+    }
+}
