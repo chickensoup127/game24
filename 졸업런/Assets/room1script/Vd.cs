9442dee99abbd012dd8808b0162d9237e0442cc2
jy222ce <55487601+jy222ce@users.noreply.github.com>
2019-12-08 18:10:14 +0900

    startvideo add

---
+++ 졸업런/Assets/room1script/Vd.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class Vd : MonoBehaviour
+{
+
+    public VideoPlayer vp;
+    public GameObject obj;
+
+
+    private void Awake()
+    {
+        Time.timeScale = 0;
+        vp.Prepare();
+        vp.Play();
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vp.time >= vp.clip.length)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                vp.Stop();
+                Time.timeScale = 1;
+                obj.SetActive(true);
+            }
+
+        }
+    }
+}
