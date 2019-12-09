ffed5241e3e4f40b90955d1d7aac7b09827d56b7
HongTaeha <xogkdmltaha1@naver.com>
2019-12-09 17:09:23 +0900

    Revert "수정"

---
+++ 졸업런/Assets/room1script/SuccessZone.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class SuccessZone : MonoBehaviour
+{
+
+    public Canvas Score;
+    //충돌
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag.CompareTo("Player") == 0)
+        {
+            DataManager.Instance.Success = true;
+            Score.enabled = false;
+            SceneManager.LoadScene("room2");
+        }
+    }
+}
---
+++ 졸업런/Assets/room1script/Vd.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class Vd : MonoBehaviour
+{
+    public Canvas Score;
+    public VideoPlayer vp;
+    public GameObject obj;
+
+
+    private void Awake()
+    {
+        Time.timeScale = 0;
+        vp.Prepare();
+        vp.Play();
+        Score.enabled = false;
+    }
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (vp.time >= vp.clip.length)
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                vp.Stop();
+                Time.timeScale = 1;
+                obj.SetActive(true);
+                Score.enabled = true;
+            }
+
+        }
+    }
+}
