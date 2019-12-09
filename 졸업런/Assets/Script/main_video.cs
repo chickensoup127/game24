d15090b20bb66ae90fdf9808f6340b352b37164f
chickensoup127 <haein127@naver.com>
2019-12-09 22:29:29 +0900

    main추가

---
+++ 졸업런/Assets/Script/main_video.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+public class main_video : MonoBehaviour
+{
+
+    public VideoPlayer start;
+    // Start is called before the first frame update
+    void Start()
+    {
+        start.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+    }
+}
